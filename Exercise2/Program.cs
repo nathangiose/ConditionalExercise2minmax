ddd831ee2b4d4ba75f0e0e799d4dcbdc00467879
nathangiose <nathangiose@gmail.com>
2021-12-24 01:57:22 +0200

    Add project files.

---
+++ Exercise2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] numbers = new int[13]; This was my first attempt
+
+            var numbers = new int[3] { 1, 2, 3 }; //corrected my syntax
+            Console.WriteLine("max = {0} \nmin = {1}", numbers.Max(), numbers.Min());
+        }
+    }
+}
